6881cb69e52d3c365a012fe81cac9c5516f357de
nandakumar.rangasamy <geeknanda13@gmail.com>
2023-05-23 13:05:04 +0530

    updated

---
+++ java-learn/practice/src/com/nanda/problem/solving/array/AllowedChars.java
@@ -0,0 +1,12 @@
+package com.nanda.problem.solving.array;
+
+public class AllowedChars {
+
+
+
+
+
+    public static void main(String[] args) {
+
+    }
+}
---
+++ java-learn/practice/src/com/nanda/problem/solving/array/SingleNumber.java
@@ -0,0 +1,40 @@
+package com.nanda.problem.solving.array;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class SingleNumber {
+
+    public int singleNumber(int[] nums) {
+
+        int sNumber = -1;
+
+        Map<Integer, Integer> counterMap = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            if (counterMap.containsKey(nums[i])) {
+                int count = counterMap.get(nums[i]);
+
+                counterMap.put(nums[i], ++count);
+            } else {
+                counterMap.put(nums[i], 1);
+            }
+        }
+
+        for (Map.Entry<Integer,Integer> entry : counterMap.entrySet()) {
+            if (entry.getValue() == 1) {
+                sNumber = entry.getKey();
+                break;
+            }
+        }
+
+            return sNumber;
+
+    }
+
+    public static void main(String[] args) {
+        new SingleNumber().singleNumber(new int[]{4,1,2,1,2});
+    }
+}
