f8849188a78aef5494a62a4dbc300b5d53066998
nandar <geeknanda13@gmail.com>
2023-05-25 16:48:16 +0530

    updated

---
+++ java-learn/practice/src/com/nanda/problem/solving/array/MaxDiffTwoPAirs.java
@@ -0,0 +1,30 @@
+package com.nanda.problem.solving.array;
+
+import java.util.Arrays;
+
+public class MaxDiffTwoPAirs {
+
+
+    public int maxProductDifference(int[] nums) {
+
+        int maxDiff = 0;
+
+        Arrays.sort(nums);
+
+        int p1 = nums[0] * nums [1];
+
+        int p2 = nums[nums.length - 1] * nums[nums.length - 2];
+
+        maxDiff = p2 - p1;
+
+
+        return maxDiff;
+
+    }
+
+    public static void main(String[] args) {
+
+        new MaxDiffTwoPAirs().maxProductDifference(new int[]{4,2,5,9,7,4,8});
+
+    }
+}
